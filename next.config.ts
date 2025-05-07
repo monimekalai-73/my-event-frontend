f86db88b7045e0f306a5e6cdbfb4eb56454d56e2
Monimekalai Saravanan <monimekalaisaravanan@Monimekalais-MacBook-Air.local>
2025-05-07 16:00:07 +0530

    fix: layout and config for Next.js build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // other config options here
 };
 
-export default nextConfig;
+module.exports = nextConfig;
