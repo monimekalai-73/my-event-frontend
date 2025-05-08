96af12c7ad510ff96f7fed0c489d76258ef6bff4
Monimekalai Saravanan <monimekalaisaravanan@Monimekalais-MacBook-Air.local>
2025-05-08 15:01:06 +0530

    Api added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { useTranslation } from 'react-i18next';
+//import { useTranslation } from 'react-i18next';
 import '../i18n'; // Import the i18n setup
+import { useEffect, useState } from 'react';
 
-export default function HomePage() {
+/*export default function HomePage() {
   const { t, i18n } = useTranslation();
 
   const isArabic = i18n.language === 'ar';
@@ -14,6 +15,22 @@ export default function HomePage() {
       <button onClick={() => i18n.changeLanguage(isArabic ? 'en' : 'ar')}>
         {isArabic ? 'Switch to English' : 'التبديل إلى العربية'}
       </button>
+    </div>
+  );
+}*/
+export default function HomePage() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <p>{message}</p>
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  //output: "export", // ✅ THIS is the key fix
+  outputStandalone: true, 
 };
 
 export default nextConfig;
